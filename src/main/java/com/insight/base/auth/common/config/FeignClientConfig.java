cae0ea7a0436d2447ccaf597068571ddcfc1e9cc
Brian <xan.brian@gmail.com>
2019-09-13 23:06:48 +0800

    优化代码

--- src/main/java/com/insight/base/auth/common/config/FeignClientConfig.java
+++ src/main/java/com/insight/base/auth/common/config/FeignClientConfig.java
@@ -1,12 +1,13 @@
 package com.insight.base.auth.common.config;
 
 import feign.RequestInterceptor;
-import org.springframework.context.annotation.Bean;
+import feign.RequestTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 /**
  * @author 宣炳刚
@@ -14,17 +15,26 @@ import javax.servlet.http.HttpServletRequest;
  * @remark
  */
 @Component
-public class FeignClientConfig {
-    @Bean
-    public RequestInterceptor headerInterceptor() {
+public class FeignClientConfig implements RequestInterceptor {
+
+    /**
+     * 应用配置
+     *
+     * @param template RequestTemplate
+     */
+    @Override
+    public void apply(RequestTemplate template) {
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (requestAttributes == null){
-            return null;
+        if (requestAttributes == null) {
+            return;
         }
 
         HttpServletRequest request = requestAttributes.getRequest();
-        String requestId = request.getHeader("requestId");
-
-        return requestTemplate -> requestTemplate.header("requestId", requestId);
+        Enumeration<String> headers = request.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String name = headers.nextElement();
+            String values = request.getHeader(name);
+            template.header(name, values);
+        }
     }
 }
